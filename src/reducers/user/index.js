0858f9863011fd5a5dbf82bd57c9ec63b00c7250
James Hall <jhall@352inc.com>
2016-07-26 10:21:43 -0400

    my stuff

--- src/reducers/user/index.js
+++ src/reducers/user/index.js
@@ -6,6 +6,10 @@ export default function user(state = null, action) {
       return { fetching: true };
     case UserActions.RECEIVE_USER:
       return action.user;
+    case UserActions.FETCH_COMPANY:
+      return { fetching: true };
+      case UserActions.RECEIVE_COMPANY:
+        return action.company;
     default:
       return state;
   }
