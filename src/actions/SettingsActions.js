9396dd30048f289a23f14d0dffed4dc398c6ef46
Jason Elgin <jason@jrelgin.com>
2016-08-01 12:43:41 -0400

    almost there

--- src/actions/SettingsActions.js
+++ src/actions/SettingsActions.js
@@ -1,3 +1,11 @@
 'use strict';
 
 const namespace = 'settings/';
+
+export const POPLULATE_SETTINGS = `${namespace}POPULATE_SETTINGS`;
+
+export let populateSettings = (user) => {
+  return {
+    type: POPULATE_SETTINGS
+  }
+}
